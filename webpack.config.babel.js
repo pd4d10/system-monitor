a454d263d876566bb3a35f74c98f35abe2d3e96c
pd4d10 <pd4d10@gmail.com>
2016-09-29 15:15:13 +0800

    add hot loader

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,7 +1,11 @@
 import webpack from 'webpack'
 
 export default {
-  entry: ['./src'],
+  entry: [
+    // 'webpack-dev-server/client?http://0.0.0.0:3000', // WebpackDevServer host and port
+    // 'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
+    './src',
+  ],
   output: {
     path: './chrome/dist',
     filename: 'popup.js',
