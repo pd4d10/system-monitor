990bad20f12d6037c579c4d8f2a9b62dfd753bb7
pd4d10 <pd4d10@gmail.com>
2017-01-22 11:28:45 +0800

    Remove bundle analyzer plugin in production

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,8 +1,13 @@
 const webpack = require('webpack')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 const config = require('./webpack.config')
 
-config.plugins.push(new webpack.DefinePlugin({
-  'process.env.NODE_ENV': JSON.stringify('production'),
-}))
+config.plugins = [
+  new CleanWebpackPlugin(['chrome/dist']),
+  new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js'),
+  new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify('production'),
+  }),
+]
 
 module.exports = config
