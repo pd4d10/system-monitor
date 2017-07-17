4b46529b122c17f1b91aae01e3896e60171a0156
pd4d10 <pd4d10@gmail.com>
2017-07-17 13:31:10 +0800

    Use svg2png to generate png

---
+++ scripts/icon.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+const path = require('path')
+const svg2png = require("svg2png")
+
+const getDir = dir => path.resolve(__dirname, dir)
+
+const file = fs.readFileSync(getDir('../src/icon.svg'))
+svg2png(file).then(buffer => {
+  fs.writeFile(getDir('../chrome/dist/icon.png'), buffer)
+})
