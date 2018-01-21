4b445ecd8f0e7c4dddeaf7da6aaad6519182c1e4
pd4d10 <pd4d10@gmail.com>
2018-01-21 19:56:28 +0800

    Fix icon path

--- scripts/icon.js
+++ scripts/icon.js
@@ -6,5 +6,5 @@ const getDir = dir => path.resolve(__dirname, dir)
 
 const file = fs.readFileSync(getDir('../src/icon.svg'))
 svg2png(file).then(buffer => {
-  fs.writeFile(getDir('../chrome/dist/icon.png'), buffer)
+  fs.writeFile(getDir('../chrome/icon.png'), buffer)
 })
