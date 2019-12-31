b0785c5cd075bb099d8f62c18947925450fd4b12
Rongjian Zhang <pd4d10@gmail.com>
2019-12-31 12:43:28 +0800

    chore: drop webpack-merge

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,14 +1,14 @@
 const webpack = require('webpack')
-const merge = require('webpack-merge')
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 const config = require('./webpack.config')
 
 /**
  * @type {import('webpack').Configuration}
  */
-module.exports = merge(config, {
+module.exports = {
+  ...config,
   mode: 'production',
   watch: false,
   devtool: false,
-  plugins: [new BundleAnalyzerPlugin()],
-})
+  plugins: [...config.plugins, new BundleAnalyzerPlugin()],
+}
