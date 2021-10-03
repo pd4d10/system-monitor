7218984888e500ff58c14c284c83ca95a0e4a619
Rongjian Zhang <pd4d10@gmail.com>
2021-10-04 01:04:14 +0800

    chore: vite mode

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,27 @@
 // @ts-check
 import { defineConfig } from 'vite'
 
-export default defineConfig({
-  base: '/dist',
-  build: {
-    rollupOptions: {
-      input: {
-        background: './background.html',
-        popup: './popup.html',
-        options: './options.html',
+export default defineConfig(({ mode }) => {
+  const prod = mode === 'production'
+  // console.log(prod)
+
+  return {
+    base: '/dist',
+    build: {
+      watch: prod ? null : {},
+      minify: prod,
+      rollupOptions: {
+        input: {
+          background: './background.html',
+          popup: './popup.html',
+          options: './options.html',
+        },
+        output: {
+          entryFileNames: '[name].js',
+          dir: 'chrome/dist',
+        },
       },
-      output: {
-        entryFileNames: '[name].js',
-        dir: 'chrome/dist',
-      },
+      sourcemap: 'inline',
     },
-    sourcemap: 'inline',
-  },
+  }
 })
