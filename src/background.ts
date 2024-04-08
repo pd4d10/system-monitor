52f04e0367eb4e285bd2c6ae9fc373f112769030
Rongjian Zhang <pd4d10@gmail.com>
2024-04-08 21:34:49 +0800

    refactor: extract constant

--- src/background.ts
+++ src/background.ts
@@ -1,10 +1,11 @@
 import { Effect, Schedule } from "effect";
 import { getSystemInfo } from "./utils";
 
-const canvas = new OffscreenCanvas(19, 19);
+const size = 19;
+const canvas = new OffscreenCanvas(size, size);
 const ctx = canvas.getContext("2d")!;
 
-const cpuIdleArray = Array.from(Array(19), () => 1);
+const cpuIdleArray = Array.from(Array(size), () => 1);
 
 const draw = ({
   processors,
@@ -22,15 +23,15 @@ const draw = ({
   cpuIdleArray.shift();
 
   // clear
-  ctx.clearRect(0, 0, 19, 19);
+  ctx.clearRect(0, 0, size, size);
 
   // background
   ctx.beginPath();
-  ctx.moveTo(0, 19);
+  ctx.moveTo(0, size);
   cpuIdleArray.forEach((v, i) => {
-    ctx.lineTo(i, v * 19);
+    ctx.lineTo(i, v * size);
   });
-  ctx.lineTo(19, 19);
+  ctx.lineTo(size, size);
   ctx.lineWidth = 2;
   ctx.fillStyle = "#4876ff";
   ctx.fill();
@@ -38,16 +39,16 @@ const draw = ({
   // border
   ctx.beginPath();
   ctx.moveTo(0, 0);
-  ctx.lineTo(0, 19);
-  ctx.lineTo(19, 19);
-  ctx.lineTo(19, 0);
+  ctx.lineTo(0, size);
+  ctx.lineTo(size, size);
+  ctx.lineTo(size, 0);
   ctx.closePath();
   ctx.lineWidth = 2;
   ctx.strokeStyle = "#1874cd";
   ctx.stroke();
 
   chrome.browserAction.setIcon({
-    imageData: ctx.getImageData(0, 0, 19, 19),
+    imageData: ctx.getImageData(0, 0, size, size),
   });
 };
 
