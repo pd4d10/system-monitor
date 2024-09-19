1d339e6765d8b091c467a6a4d559e4efb6e9b2af
Rongjian Zhang <pd4d10@gmail.com>
2024-09-19 13:58:40 +0800

    refactor: array fill

--- memory-monitor/src/background.ts
+++ memory-monitor/src/background.ts
@@ -1,26 +1,18 @@
 import { runOnceOnStartup, setActionIcon } from "utils";
 
-var SIZE = 19; // Icon size
-
-// Define an array to storage available memory percent
-var availMem = Array(SIZE);
-for (var i = availMem.length; i--;) {
-  availMem[i] = 1;
-}
+const data = Array(19).fill(1);
 
 function draw() {
-  // Get available memory percent
-  chrome.system.memory.getInfo(function(info) {
-    availMem.push(info.availableCapacity / info.capacity);
-    availMem.shift();
+  chrome.system.memory.getInfo((info) => {
+    data.push(info.availableCapacity / info.capacity);
+    data.shift();
 
-    // Show memory information on mouse over
     chrome.action.setTitle({
       title: "Total: " + (info.capacity / 1073741824).toFixed(2) + " GiB\n"
         + "Available: " + (info.availableCapacity / 1073741824).toFixed(2) + " GiB",
     });
 
-    setActionIcon(availMem, {
+    setActionIcon(data, {
       color: "#66cdaa",
       borderColor: "#008744",
     });
--- system-monitor/src/background.ts
+++ system-monitor/src/background.ts
@@ -1,24 +1,7 @@
 import { runOnceOnStartup, setActionIcon } from "utils";
 import { getSystemInfo } from "./utils";
 
-const SIZE = 19;
-
-const config = {
-  cpu: {
-    border: "#1874cd",
-    background: "#4876ff",
-  },
-};
-
-// 3 => [1, 1, 1]
-function fill(count: number) {
-  const arr = [];
-  for (let i = 0; i < count; i += 1) {
-    arr.push(1);
-  }
-  return arr;
-}
-const cpuIdleArray = fill(SIZE);
+const data = Array(19).fill(1);
 
 runOnceOnStartup(() => {
   getSystemInfo(({ cpu: { modelName, usage } }: {
@@ -28,15 +11,15 @@ runOnceOnStartup(() => {
     };
   }) => {
     const idle = usage.reduce((a, b) => a + b.idle / b.total, 0) / usage.length;
-    cpuIdleArray.push(idle);
-    cpuIdleArray.shift();
+    data.push(idle);
+    data.shift();
 
     chrome.action.setTitle({
       title: `${modelName}\nUsage: ${(100 * (1 - idle)).toFixed(0)}%`,
     });
-    setActionIcon(cpuIdleArray, {
-      color: config.cpu.background,
-      borderColor: config.cpu.border,
+    setActionIcon(data, {
+      color: "#4876ff",
+      borderColor: "#1874cd",
     });
   });
 });
